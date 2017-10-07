b602a39079a49b8fa7aa809233bfcd65dc2f2d50
Mohd Samiullah <amsify42@gmail.com>
2017-10-07 23:49:11 +0530

    added some test files

---
+++ test/test.php
@@ -0,0 +1,9 @@
+<?php
+
+header('Content-Type: application/json');
+sleep(3);
+echo json_encode(array(
+				'status'	=> 'success',
+				'title' 	=> 'Test Title',
+				'html' 		=> 'myectomy gearwheel psalterial misconfer machar monadelph lumbricine unobscure lightning wankliness neuronymy hypoionian irrecusable visceromotor roostership velamentous unomitted sublimation tapacolo unpreserved hematoidin postthoracic crinula phosphoglycerateTest Content'
+			));
